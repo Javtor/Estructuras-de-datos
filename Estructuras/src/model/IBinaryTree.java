990288e20c62f210872525bb79bebae68b84832a
camilaleniss <mlmariacami2@gmail.com>
2018-11-11 14:34:36 -0500

    Update con las estructuras implementadas en el laboratorio 3
    Añadido BinaryTree, AVLTreey RBTree

--- Estructuras/src/model/IBinaryTree.java
+++ Estructuras/src/model/IBinaryTree.java
@@ -1,11 +1,35 @@
 package model;
 
-public interface IBinaryTree<T> {
+import java.util.ArrayList;
 
-	public void addNode(T node);
+public interface IBinaryTree<K extends Comparable<K>, V> {
+
+	public void insert(K key, V value);
 	
-	public NodeBinaryTree<T> deleteNode(T node);
+	public V delete(K key);
 	
-	public NodeBinaryTree<T> searchNode(T node);
+	public V delete (K key, V value);
+	
+	public V search(K key);
+	
+	public boolean isInTree(K key);
+	
+	public K getMin();
+		
+	public K getMax();
+	
+	public K getPredecessor(K key);
+	
+	public K getSuccessor(K key);
+	
+	public ArrayList<V> searchEqualTo(K key);
+	
+	public ArrayList<V> searchLowerOrEqualTo(K key);
+	
+	public ArrayList<V> searchLowerTo(K key);
+	
+	public ArrayList<V> searchBiggerOrEqualThan(K key);
+	
+	public ArrayList<V> searchBiggerThan(K key);
 	
 }
--- Estructuras/src/model/NodeBinaryTree.java
+++ Estructuras/src/model/NodeBinaryTree.java
@@ -1,36 +1,59 @@
 package model;
 
-public class NodeBinaryTree<T> {
+public class NodeBinaryTree<K extends Comparable<K>, V>{
 
-	private T value;
-	private Node<T> parent;
-	private Node<T> left;
-	private Node<T> right;
+	private K key;
+	private V value;
+	private NodeBinaryTree<K, V> parent;
+	private NodeBinaryTree<K, V> left;
+	private NodeBinaryTree<K, V> right;
 	
-	public 	NodeBinaryTree(T value) {
+	public 	NodeBinaryTree(K key, V value) {
+		this.key = key;
 		this.value = value;
 		left=null;
 		right=null;
+		parent = null;
 	}
 	
-	public T getValue() {
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public V getValue() {
 		return value;
 	}
 
-	public Node<T> getRight() {
-		return right;
+	public void setValue(V value) {
+		this.value = value;
 	}
 
-	public void setRight(Node<T> right) {
-		this.right = right;
+	public NodeBinaryTree<K, V> getParent() {
+		return parent;
 	}
 
-	public Node<T> getLeft() {
+	public void setParent(NodeBinaryTree<K, V> parent) {
+		this.parent = parent;
+	}
+
+	public NodeBinaryTree<K, V> getLeft() {
 		return left;
 	}
 
-	public void setLeft(Node<T> left) {
+	public void setLeft(NodeBinaryTree<K, V> left) {
 		this.left = left;
+	}
+
+	public NodeBinaryTree<K, V> getRight() {
+		return right;
+	}
+
+	public void setRight(NodeBinaryTree<K, V> right) {
+		this.right = right;
 	}
 
 	public boolean isLeaf() {
